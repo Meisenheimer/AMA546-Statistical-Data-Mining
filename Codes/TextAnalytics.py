d5ed640c0d5f0cac2645b3cd6fa8bf48e9603cce
WANG Zeyu <zeyu.wang.0117@outlook.com>
2025-03-10 17:32:46 +0800

    feat: add stop words for text processing

--- Codes/TextAnalytics.py
+++ Codes/TextAnalytics.py
@@ -1,8 +1,12 @@
 from tqdm import tqdm
 import numpy as np
 
+with open("../Data/stop_words_english.txt", "r", encoding="UTF-8") as fp:
+    data = fp.read()
+    STOP_WORDS = set(data.split('\n'))
 
-def get_vocabulary(words_dict: dict, keys: list) -> dict:
+
+def get_vocabulary(words_dict: dict, keys: list, del_stop_words: bool = False) -> dict:
     """
     compute the vocabulary for tokens
     """
@@ -11,23 +15,27 @@ def get_vocabulary(words_dict: dict, keys: list) -> dict:
     for key in tqdm(keys):
         for item in words_dict[key]:
             if (item not in vocabulary.keys()):
-                vocabulary[item] = num
-                num += 1
+                if ((not del_stop_words) or (item not in STOP_WORDS)):
+                    vocabulary[item] = num
+                    num += 1
     if (len(vocabulary) != num):
+        raise
+    if ("" in vocabulary or "\n" in vocabulary or " " in vocabulary):
         raise
     return vocabulary
 
 
 def calc_tf_idf(vocabulary: dict, words_dict: dict, keys: list) -> tuple:
     """
-    compute the tf_idf for tokens
+    compute the tf, idf, tf_idf for tokens
     """
     tf = np.zeros((len(words_dict), len(vocabulary)))
     keys = list(keys)
     for i in tqdm(range(len(keys))):
         key = keys[i]
         for item in words_dict[key]:
-            tf[i, vocabulary[item]] += 1.0
+            if (item in vocabulary):
+                tf[i, vocabulary[item]] += 1.0
     idf = np.log2(float(tf.shape[0]) / np.count_nonzero(tf, axis=0))
     tf_idf = tf * idf
     return tf, idf, tf_idf
